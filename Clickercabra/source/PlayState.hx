ee9b9881353ba60f0a3f8e2d9110b3e0dd71879a
Momin Khan <foolmoron@gmail.com>
2015-08-22 15:45:02 -0400

    Added fully functioning BigNum class and removed extra build stuff from repo

--- Clickercabra/source/PlayState.hx
+++ Clickercabra/source/PlayState.hx
@@ -5,35 +5,73 @@ import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
+import flixel.util.FlxTimer;
 import flixel.util.FlxMath;
 
-/**
- * A FlxState which can be used for the actual gameplay.
- */
+
 class PlayState extends FlxState
 {
-	/**
-	 * Function that is called up when to state is created to set it up. 
-	 */
+	var testText:FlxText;
+	var time:FlxTimer = new FlxTimer(100);
+	var x:Int;
+	var b:BigNum = new BigNum();
+
 	override public function create():Void
 	{
+		FlxG.autoPause = false; // it's an idler, so let it always play
+
+		//make border
+		{
+			var borderSize = 10;
+			var border = new FlxSprite(0, 0);
+			border.makeGraphic(FlxG.width, FlxG.height, 0xFFFF0000);
+			add(border);
+			var background = new FlxSprite(borderSize, borderSize);
+			background.makeGraphic(FlxG.width - (borderSize*2), FlxG.height - (borderSize*2), 0x80000000);
+			add(background);
+		}
+		// thing
+		{
+			add(new FlxSprite(100, 100).makeGraphic(10, 20, 0xFF00FF00));
+		}
+		// grid
+		{
+			var cellSize = 10;
+			for (x in 0...Std.int(FlxG.width/cellSize)) {
+				var vertLine = new FlxSprite(x * cellSize, 0);
+				vertLine.makeGraphic(1, FlxG.height, 0xFFFFFFFF);
+				add(vertLine);
+			}
+			for (y in 0...Std.int(FlxG.height/cellSize)) {
+				var horizLine = new FlxSprite(0, y * cellSize);
+				horizLine.makeGraphic(FlxG.width, 1, 0xFFFFFFFF);
+				add(horizLine);
+			}
+		}
+		// text
+		{
+			this.testText = new FlxText(30, 150, 0, null, 16);
+			this.testText.setBorderStyle(FlxText.BORDER_OUTLINE, 0x000000, 2, 1);
+			this.testText.color = 0xFFFFFFFF;
+			add(this.testText);
+		}
+
 		super.create();
 	}
-	
-	/**
-	 * Function that is called when this state is destroyed - you might want to 
-	 * consider setting all objects this state uses to null to help garbage collection.
-	 */
+
 	override public function destroy():Void
 	{
 		super.destroy();
 	}
 
-	/**
-	 * Function that is called once every frame.
-	 */
 	override public function update():Void
 	{
+		var val = FlxG.keys.pressed.G ? 921070 : 500;
+		val *= FlxG.keys.pressed.T ? 100 : 1;
+		var sign = FlxG.keys.pressed.F ? -1 : 1;
+		b = b.addNum(val * sign);
+		this.testText.text = b.toString();
+
 		super.update();
 	}	
 }
